8ddb2528ec6ae9555bbf96dd040918c04d7506dd
Sean8069 <seanlau8069@gmail.com>
2021-06-06 17:37:56 +0800

    finalizing

--- User_Account_Creation.sh
+++ User_Account_Creation.sh
@@ -6,30 +6,45 @@
 
 
 # Make sure the script is being executed with superuser privileges.
-if [[ ${UID} -ne 0 ]]
+if [[ "${UID}" -ne 0 ]]
 then
-	echo 'You are not a root user, exiting program...'
-	exit 1
+   echo 'Please run with sudo or as root.'
+   exit 1
 fi
 
 # Get the username (login).
-read -p 'Please enter the desired username: ' ${USERNAME}
+read -p 'Enter the username to create: ' USER_NAME
 
 # Get the real name (contents for the description field).
-read -p 'Please enter the name of the person who this account is for: '${COMMENT}
+read -p 'Enter the name of the person or application that will be using this account: ' COMMENT
 
 # Get the password.
-read -p 'Please set a password for the user: ' ${PASSWORD}
+read -p 'Enter the password to use for the account: ' PASSWORD
 
-# Create the user with the password.
-useradd -c "${USERNAME}" -m $USERNAME
+# Create the account.
+useradd -c "${COMMENT}" -m ${USER_NAME} -p ${PASSWORD}
 
 # Check to see if the useradd command succeeded.
+# We don't want to tell the user that an account was created when it hasn't been.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The account could not be created.'
+  exit 1
+fi
 
-# Set the password.
-
-# Check to see if the passwd command succeeded.
 
 # Force password change on first login.
+passwd -e ${USER_NAME}
 
 # Display the username, password, and the host where the user was created.
+echo
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
+echo
+echo 'host:'
+echo "${HOSTNAME}"
+exit 0
+
