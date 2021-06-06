20f4e5d3162b545e433d9f1e615367f02e99fb9c
Sean8069 <seanlau8069@gmail.com>
2021-06-06 16:47:12 +0800

    added useradd

--- User_Account_Creation.sh
+++ User_Account_Creation.sh
@@ -8,19 +8,21 @@
 # Make sure the script is being executed with superuser privileges.
 if [[ ${UID} -ne 0 ]]
 then
-	echo "You are not a root user, exiting program..."
+	echo 'You are not a root user, exiting program...'
 	exit 1
-else
-	echo "You are root user, please continue."
 fi
 
 # Get the username (login).
+read -p 'Please enter the desired username: ' ${USERNAME}
 
 # Get the real name (contents for the description field).
+read -p 'Please enter the name of the person who this account is for: '${COMMENT}
 
 # Get the password.
+read -p 'Please set a password for the user: ' ${PASSWORD}
 
 # Create the user with the password.
+useradd -c "${USERNAME}" -m $USERNAME
 
 # Check to see if the useradd command succeeded.
 
